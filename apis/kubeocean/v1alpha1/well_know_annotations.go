53827ac8ed673b5113ef197291955a17da92c948
litao <taoli@yunify.com>
2022-07-07 11:11:55 +0800

    add ks install timestamp annotation

--- apis/kubeocean/v1alpha1/well_know_annotations.go
+++ apis/kubeocean/v1alpha1/well_know_annotations.go
@@ -1,8 +1,9 @@
 package v1alpha1
 
 const (
-	ResourceObjectNameAnnotation          = "vcluster.loft.sh/object-name"
-	IngressNginxBackendProtocolAnnotation = "nginx.ingress.kubernetes.io/backend-protocol"
-	IngressNginxSslPassthroughAnnotation  = "nginx.ingress.kubernetes.io/ssl-passthrough"
-	IngressNginxSslRedirectAnnotation     = "nginx.ingress.kubernetes.io/ssl-redirect"
+	ResourceObjectNameAnnotation                       = "vcluster.loft.sh/object-name"
+	IngressNginxBackendProtocolAnnotation              = "nginx.ingress.kubernetes.io/backend-protocol"
+	IngressNginxSslPassthroughAnnotation               = "nginx.ingress.kubernetes.io/ssl-passthrough"
+	IngressNginxSslRedirectAnnotation                  = "nginx.ingress.kubernetes.io/ssl-redirect"
+	DolphinClusterInstallKubesphereTimestampAnnotation = "kubeocean.kubesphere.io/dolphincluster-install-kubesphere-timestamp"
 )
